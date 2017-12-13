a28ba874b17a2ed51f19b85daeafc33f24dc6f74
Ievgen <yevgens@web.de>
2017-12-13 08:03:18 +0200

    add 'Г-н' / 'Г-жа', without decorators...

--- problem1_1.py
+++ problem1_1.py
@@ -1,18 +1,12 @@
 # -*- coding: utf-8 -*-
 
-# def sorting(contacts):
-#     def wrapped():
-#         return contacts().sort(key = lambda i: i[-1])
-#     return wrapped
-
 def greeting():
     lst = [raw_input().split() for i in range(int(raw_input()))]
     lst.sort(key=lambda i: i[-1])
     for i in lst:
-        if i[-4] == u'М':
-            print "Г-жа", " ".join(i[0:2])
-        else:
+        if i[-2] == u'М'.encode('utf-8'):
             print "Г-н", " ".join(i[0:2])
-
+        else:
+            print "Г-жа", " ".join(i[0:2])
 
 greeting()
